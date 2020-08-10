89d27d7e36392c093287d1140a2b9669fba0a699
TheMushrr00m <funji_2302@outlook.es>
2020-08-10 05:15:08 -0500

    Setting up the project

---
+++ lib/resuelve_cc.ex
@@ -0,0 +1,18 @@
+defmodule ResuelveCc do
+  @moduledoc """
+  Documentation for `ResuelveCc`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ResuelveCc.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/resuelve_cc_test.exs
@@ -0,0 +1,8 @@
+defmodule ResuelveCcTest do
+  use ExUnit.Case
+  doctest ResuelveCc
+
+  test "greets the world" do
+    assert ResuelveCc.hello() == :world
+  end
+end
